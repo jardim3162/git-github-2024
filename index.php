75e0ed8bd0a96daebb7dcdb6e4e91b90392a0c8a
JardimG <joao.2020316204@aluno.iffar.edu.br>
2024-03-04 16:58:45 -0300

    criei a tabela das informações

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Pagina inicial</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Email</th>
+                <th colspan="2">Opções</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Thiago</td>
+                <td>Alterar</td>
+                <td>Excluir</td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+
+</html>
